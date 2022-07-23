4e32616971e204ad4b1ff16fd624da579c84ac68
JIH0LEE <skql5810@gmail.com>
2022-07-23 16:11:18 +0900

    backend - make test api

---
+++ backend/src/main/java/io/gurumi/core/controller/ApiTestController.java
@@ -0,0 +1,15 @@
+package io.gurumi.core.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiTestController {
+    @GetMapping("/test")
+    public String test(){
+        return "success";
+    }
+}
