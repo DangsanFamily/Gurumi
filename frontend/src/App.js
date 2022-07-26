68fcd187ae6ed2a4e721e97bed9c917e7badaf8a
JIH0LEE <skql5810@gmail.com>
2022-07-26 19:40:05 +0900

    cancel merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,21 @@
-import MainPage from './Page/MainPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import MainPage from "./Page/MainPage";
+import CreateMessagePage from "./Page/CreateMessagePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App" style={{ background: 'url(./img/main.jpg)' }}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  )
+    return (
+        <div className="App" style={{ background: "url(./img/main.jpg)" }}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/create-message" element={<CreateMessagePage />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/Page/MainPage/index.js
+++ frontend/src/Page/MainPage/index.js
@@ -1,22 +1,23 @@
-import '../style.css'
-import { Button } from 'react-bootstrap'
-import axios from 'axios'
-
+import React from "react";
+import "../style.css";
+import { useNavigate } from "react-router-dom";
 function MainPage() {
-
-  const apiTest=()=>{
-   axios.get('/api/test').then(res => {
-    alert(res.data)
-  })
-  }
-
-  return (
-    <div className="main-page">
-      <div className="title">구르미</div>
-      <div className="body-container">
-        <Button className="start-button" onClick={apiTest}>시작하기</Button>
-      </div>
-    </div>
-  )
+    const next = () => {
+        navigate("/create-message");
+    };
+    const navigate = useNavigate();
+    return (
+        <div className="main-page">
+            <div className="title">
+                {" "}
+                <img className="logo" src="./img/logo.png" alt=""></img>구르미
+            </div>
+            <div className="body-container">
+                <button className="start-button hover" onClick={next}>
+                    시작하기
+                </button>
+            </div>
+        </div>
+    );
 }
-export default MainPage
+export default MainPage;
