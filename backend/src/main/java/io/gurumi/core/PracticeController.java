88135a30e060c7906e5c2c214096396c71835694
손지원 <sonjiwon@sonjiwon-ui-MacBookAir.local>
2022-07-28 21:58:56 +0900

    create test api

---
+++ backend/src/main/java/io/gurumi/core/PracticeController.java
@@ -0,0 +1,15 @@
+package io.gurumi.core;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class PracticeController {
+
+    @GetMapping
+    public String helloSpring(){
+        return "Hello Spring";
+    }
+}
