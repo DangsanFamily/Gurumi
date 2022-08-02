92a8635d8e7237e008cca27379179d47f2c0aeb0
Eugine Park <pkeugine@gmail.com>
2022-08-02 16:28:07 +0900

    refactor BlockController

--- backend/src/main/java/io/gurumi/core/blocks/service/BlockService.java
+++ backend/src/main/java/io/gurumi/core/blocks/service/BlockService.java
@@ -8,30 +8,33 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class BlockService {
+
     private final BlockRepository blockRepository;
 
-    public BlockService(BlockRepository blockRepository){
-        this.blockRepository=blockRepository;
+    public BlockService(BlockRepository blockRepository) {
+        this.blockRepository = blockRepository;
     }
-    public BlockResponse readBlock(Long blockId){
-        Block block=blockRepository.findById(blockId).get();
+
+    public BlockResponse readBlock(Long blockId) {
+        Block block = blockRepository.findById(blockId).get();
         return block.toResponse();
 
     }
 
-    public BlockResponse createBlock(BlockRequest blockRequest){
-        Block block=blockRequest.toEntity();
+    public BlockResponse createBlock(BlockRequest blockRequest) {
+        Block block = blockRequest.toEntity();
         blockRepository.save(block);
         return block.toResponse();
     }
-    public BlockResponse updateBlock(BlockRequest blockRequest,Long blockId){
-        Block block=blockRepository.findById(blockId).get();
+
+    public BlockResponse updateBlock(Long blockId, BlockRequest blockRequest) {
+        Block block = blockRepository.findById(blockId).get();
         block.setContent(blockRequest.getContent());
         blockRepository.save(block);
         return block.toResponse();
     }
 
-    public void deleteBlock(Long blockId){
+    public void deleteBlock(Long blockId) {
         blockRepository.deleteById(blockId);
     }
 }
--- backend/src/main/java/io/gurumi/core/blocks/ui/BlockController.java
+++ backend/src/main/java/io/gurumi/core/blocks/ui/BlockController.java
@@ -3,9 +3,16 @@ package io.gurumi.core.blocks.ui;
 import io.gurumi.core.blocks.service.BlockService;
 import io.gurumi.core.blocks.ui.dto.BlockRequest;
 import io.gurumi.core.blocks.ui.dto.BlockResponse;
-import org.springframework.http.HttpStatus;
+import java.net.URI;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/blocks")
@@ -13,30 +20,32 @@ public class BlockController {
 
     private final BlockService blockService;
 
-    public BlockController(BlockService blockService){
-        this.blockService=blockService;
+    public BlockController(BlockService blockService) {
+        this.blockService = blockService;
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<BlockResponse> readBlock(@PathVariable(name="id") Long blockId){
-        return ResponseEntity.status(HttpStatus.OK).body(blockService.readBlock(blockId));
+    public ResponseEntity<BlockResponse> readBlock(@PathVariable Long id) {
+        BlockResponse blockResponse = blockService.readBlock(id);
+        return ResponseEntity.ok(blockResponse);
     }
+
     @PostMapping
-    public ResponseEntity<BlockResponse> createBlock(@RequestBody BlockRequest blockRequest){
-        return ResponseEntity.status(HttpStatus.CREATED).body(blockService.createBlock(blockRequest));
+    public ResponseEntity<BlockResponse> createBlock(@RequestBody BlockRequest blockRequest) {
+        URI location = URI.create("abcd");
+        BlockResponse blockResponse = blockService.createBlock(blockRequest);
+        return ResponseEntity.created(location).body(blockResponse);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<BlockResponse> updateBlock(@RequestBody BlockRequest blockRequest,@PathVariable(name="id") Long blockId){
-        return ResponseEntity.status(HttpStatus.OK).body(blockService.updateBlock(blockRequest,blockId));
+    public ResponseEntity<BlockResponse> updateBlock(@PathVariable Long id, @RequestBody BlockRequest blockRequest) {
+        BlockResponse blockResponse = blockService.updateBlock(id, blockRequest);
+        return ResponseEntity.ok(blockResponse);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBlock(@PathVariable(name="id") Long blockId){
-        blockService.deleteBlock(blockId);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-
+    public ResponseEntity<Void> deleteBlock(@PathVariable Long id) {
+        blockService.deleteBlock(id);
+        return ResponseEntity.noContent().build();
     }
-
-
 }
