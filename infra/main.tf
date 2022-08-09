fd862e87117a29b59b9b56abf803a10f847b7ffe
Eugine Park <pkeugine@gmail.com>
2022-08-09 15:33:57 +0900

    create default aws resources

---
+++ infra/main.tf
@@ -0,0 +1,33 @@
+terraform {
+    required_providers {
+       aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.16"
+       }
+    }
+
+    required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+    profile = "default"
+    region = "ap-northeast-2"
+}
+
+resource "aws_instance" "dev_app_server" {
+    ami           = "ami-0ea5eb4b05645aa8a"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "DevAppServer"
+    }
+}
+
+resource "aws_instance" "dev_nginx_server" {
+    ami           = "ami-0ea5eb4b05645aa8a"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "DevNginxServer"
+    }
+}
